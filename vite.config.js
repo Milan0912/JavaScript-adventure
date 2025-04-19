b76a67b642f1eda7739ec98d93e0151553cfc366
Milan0912 <milandhiman005@gmail.com>
2025-04-19 14:10:59 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/JavaScript-adventure/', // this is needed for GitHub Pages
+  base: '/JavaScript-adventure/', // <- This must match your repo name
 });
