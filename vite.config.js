76a601e318edf642ebbdd5c3a129d458ba133758
milan <milandhiman005@gmail.com>
2025-05-14 22:05:36 +0530

    fix: added react-router-dom and externalized in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // Correct base path for relative URLs
+  base: './', // Important for relative paths in production
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],  // Add this line if Vercel is not resolving it
+    },
+  },
   plugins: [react()],
 })
